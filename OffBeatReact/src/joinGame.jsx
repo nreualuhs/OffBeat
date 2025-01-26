a401606b1b2a9719a462fd034057b75dbb047767
Lauren Shu <113384782+nreualuhs@users.noreply.github.com>
2025-01-26 04:59:05 -0800

    join host screen

--- OffBeatReact/src/joinGame.jsx
+++ OffBeatReact/src/joinGame.jsx
@@ -1,18 +1,22 @@
 import { Link } from 'react-router-dom';
+import "./JoinGame.css";
+
 function JoinGame() {
     return(
         <>
-            <img src="./resources/Title.png" alt="OffBeat Title" height="350"></img>
-            <button><Link to="/TimerPage">PLAY GAME</Link></button>
-            <h2>Look who joined!</h2>
+            <div className="container">
+            <img src="./resources/Title.png" alt="OffBeat Title" height="340"></img>
+            <br></br>
+            <button><Link to="/TimerPage">Start Game</Link></button>
+            <h2 className="header2">Who's in?</h2>
             <div>
-                <ul>
+                <ul className="List">
                     <li>name 1</li>
                     <li>name 2</li>
                     <li>name 3</li>
                 </ul>
             </div>
-            
+            </div>
         </>
     );
 }
