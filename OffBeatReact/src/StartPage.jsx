5d6e4874db6694764ed94d05af21936d31328114
Joseph Nguyen <janhqnguyen2020@gmail.com>
2025-01-25 21:12:28 -0800

    add startpage

---
+++ OffBeatReact/src/StartPage.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+function StartPage() {
+    
+    return(
+        <div>
+            <h1>Welcome to OffBeat!</h1>
+            <h2>Instructions: find the imposter who is listening to different music!</h2>
+            <Link to="/JoinGame">START</Link>
+        </div>
+    );
+}
+
+export default StartPage
